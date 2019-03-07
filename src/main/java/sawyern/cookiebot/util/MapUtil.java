515f3cfd8ed81b2ce12e9fcc79b904ef91718e9c
Sawyer Novak <sawyern@me.com>
2019-03-06 20:28:39 -0500

    leaderboard, bugfix

--- src/main/java/sawyern/cookiebot/util/MapUtil.java
+++ src/main/java/sawyern/cookiebot/util/MapUtil.java
@@ -1,14 +1,11 @@
 package sawyern.cookiebot.util;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MapUtil {
     public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
         List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.comparingByValue());
+        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
 
         Map<K, V> result = new LinkedHashMap<>();
         for (Map.Entry<K, V> entry : list) {
