2488f674eb0bc3709ad48d508ca2d13e016c3928
Sawyer Novak <sawyern@me.com>
2019-06-17 19:16:22 -0400

    dependency fix

--- src/main/java/sawyern/cookiebot/controllers/WorldBossController.java
+++ src/main/java/sawyern/cookiebot/controllers/WorldBossController.java
@@ -1,6 +1,6 @@
 package sawyern.cookiebot.controllers;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +12,9 @@ import sawyern.cookiebot.services.WorldBossService;
 
 @RestController
 @RequestMapping(value = "api/cookiebot/v1/worldboss")
-@RequiredArgsConstructor
 public class WorldBossController {
 
-    private final WorldBossService worldBossService;
+    private WorldBossService worldBossService;
 
     @GetMapping("/spawn")
     public void spawnWorldBoss() {
@@ -30,5 +29,10 @@ public class WorldBossController {
     @GetMapping
     public ResponseEntity<WorldBoss> getWorldBoss() throws CookieException {
         return ResponseEntity.ok(worldBossService.getCurrentBoss());
+    }
+
+    @Autowired
+    public void setWorldBossService(WorldBossService worldBossService) {
+        this.worldBossService = worldBossService;
     }
 }
