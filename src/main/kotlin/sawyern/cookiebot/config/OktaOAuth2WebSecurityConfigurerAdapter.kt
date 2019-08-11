a0f6ba5cd4bd91a17582740ba581547e9683d92f
Sawyer Novak <sawyern@me.com>
2019-08-11 10:18:26 -0400

    update security whitelist

--- src/main/kotlin/sawyern/cookiebot/config/OktaOAuth2WebSecurityConfigurerAdapter.kt
+++ src/main/kotlin/sawyern/cookiebot/config/OktaOAuth2WebSecurityConfigurerAdapter.kt
@@ -1,16 +1,30 @@
 package sawyern.cookiebot.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
+@EnableWebSecurity
 class OktaOAuth2WebSecurityConfigurerAdapter: WebSecurityConfigurerAdapter() {
-    override fun configure(web: WebSecurity?) {
+
+    override fun configure(http: HttpSecurity?) {
+        if (http == null)
+            throw SecurityException("HttpSecurity not found")
+
         http
                 .authorizeRequests()
-                .antMatchers("/api/**").authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .oauth2ResourceServer().jwt()
+                .oauth2Login()
+    }
+
+    override fun configure(web: WebSecurity?) {
+        if (web == null)
+            throw SecurityException("WebSecurity not found")
+        web.ignoring().antMatchers("/test/ping")
     }
 }
