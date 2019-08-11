784b7be984311fddb2108a45b911bd733b8a4e89
Sawyer Novak <sawyern@me.com>
2019-08-11 13:44:44 -0400

    fix security

--- src/main/kotlin/sawyern/cookiebot/config/OktaOAuth2WebSecurityConfigurerAdapter.kt
+++ src/main/kotlin/sawyern/cookiebot/config/OktaOAuth2WebSecurityConfigurerAdapter.kt
@@ -1,13 +1,15 @@
 package sawyern.cookiebot.config
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
-@EnableWebSecurity
+@EnableOAuth2Sso
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 class OktaOAuth2WebSecurityConfigurerAdapter: WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
@@ -16,6 +18,8 @@ class OktaOAuth2WebSecurityConfigurerAdapter: WebSecurityConfigurerAdapter() {
 
         http
                 .authorizeRequests()
+                .antMatchers("/test/secure/**", "/api/**")
+                .hasAuthority("Admin")
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -25,6 +29,6 @@ class OktaOAuth2WebSecurityConfigurerAdapter: WebSecurityConfigurerAdapter() {
     override fun configure(web: WebSecurity?) {
         if (web == null)
             throw SecurityException("WebSecurity not found")
-        web.ignoring().antMatchers("/test/ping")
+        web.ignoring().antMatchers("/test/ping", "/home")
     }
 }
--- src/main/kotlin/sawyern/cookiebot/controllers/TestController.kt
+++ src/main/kotlin/sawyern/cookiebot/controllers/TestController.kt
@@ -1,15 +1,25 @@
 package sawyern.cookiebot.controllers
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.security.Principal
+import org.springframework.security.core.context.SecurityContextHolder
+import kotlin.math.log
+
 
 @RestController
 @RequestMapping("/test")
 class TestController {
     @GetMapping("/secure/ping")
     fun hello(principal: Principal): String {
+        return "hello ${principal.name}"
+    }
+
+    @GetMapping("/nonsecure/ping")
+    fun hello2(principal: Principal): String {
+        print(SecurityContextHolder.getContext().authentication)
         return "hello ${principal.name}"
     }
 
